98c628cee20aaf1c2ba53a03743947937ac68b2c
Norbert <norbert.kiss.89@gmail.com>
2021-01-07 15:47:39 +0100

    anchor scroll,correction index

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,47 +13,6 @@
       backDelay: 2000
     });
   }
-
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      e.preventDefault();
-      var target = $(this.hash);
-      if (target.length) {
-
-        var scrollto = target.offset().top;
-
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
-        }
-        return false;
-      }
-    }
-  });
-
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
-
   $(document).on('click', '.mobile-nav-toggle', function(e) {
     $('body').toggleClass('mobile-nav-active');
     $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
@@ -91,4 +50,21 @@
       }
     });
   });
+  $(document).ready(function(){
+    $("li a").click(function(event){
+      event.preventDefault();
+      $("html, body").animate({scrollTop: $($(this).
+        attr("href")).offset().top},1500);
+    });
+  });
+  $(document).ready(function(){
+    $('#home').click(function() {
+      $('body').removeClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500);
+    return false;
+  });
+});
 })(jQuery);
